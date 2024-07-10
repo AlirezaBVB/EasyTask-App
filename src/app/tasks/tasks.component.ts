6e83a865d1ff6adea3aef7ab4a1293b7719a46c4
AlirezaBVB <Alirezaxz5p@gmail.com>
2024-07-10 11:56:32 +0330

    Tasks-comp & Task-comp getter

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,14 +1,28 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
+import { dummyTasks } from "../dummy-tasks";
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({ required: true }) user?: User;
   //We Can Write Like That:
   // @Input() name?: string | undefined;
+
+  tasks = dummyTasks
+
+  get userTasks() {
+    return this.tasks.filter((task) => task.userId === this.user?.id)
+  }
 }
