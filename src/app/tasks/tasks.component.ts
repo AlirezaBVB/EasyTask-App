41f0dd90894cbc57dec710994202d4a65b717d30
AlirezaBVB <Alirezaxz5p@gmail.com>
2024-07-12 10:47:24 +0330

    new-task(styling)

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,18 +2,18 @@ import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { dummyTasks } from "../dummy-tasks";
 import { type User } from '../user/user.model';
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input({ required: true }) user?: User;
-  //We Can Write Like That:
-  // @Input() name?: string | undefined;
+  isAddingTask = false;
 
   tasks = dummyTasks
 
@@ -23,5 +23,13 @@ export class TasksComponent {
 
   onCompleteTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id)
+  }
+
+  onStartAddTask(){
+    this.isAddingTask = !this.isAddingTask;
+  }
+
+  onCancelAddTask(){
+    this.isAddingTask = false;
   }
 }
